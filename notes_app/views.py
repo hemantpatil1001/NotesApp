bbc85d09d49315bd648710b00610c088621b5957
hemantpatil1001 <hemantpatil1001@gmail.com>
2021-03-11 14:21:16 -0500

    Notes App Django backend

---
+++ notes_app/views.py
@@ -0,0 +1,13 @@
+# Create your views here.
+
+from rest_framework import generics
+from . import models
+from . import serializers
+
+class NoteList(generics.ListCreateAPIView):
+    queryset = models.Note.objects.all()
+    serializer_class = serializers.NoteSerializer
+
+class NoteDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = models.Note.objects.all()
+    serializer_class = serializers.NoteSerializer
